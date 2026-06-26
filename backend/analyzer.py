313aa8774dd4c73215973e72e3d07b34986cb435
HiBye-svg <562225@my.cuhsd.org>
2026-06-26 10:39:35 -0700

    Revert frame skipping for Hugging Face

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -253,11 +253,6 @@ def analyze_run(video_path, run_type="road"):
             break
 
         frame_number += 1
-
-        # Only analyze every 5th frame to reduce memory/CPU use
-        if frame_number % 10 != 0:
-            continue
-
         timestamp = 0 if fps == 0 else frame_number / fps
 
         # Resize frame before running YOLO to reduce memory use
